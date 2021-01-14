51d05d3de8f28d37609952541cbf97a589b2b9b1
Chris Eccleston <EcclestonC@gmail.com>
2021-01-14 01:24:36 -0500

    Change vs to at to denote home team

--- src/CloseGame.js
+++ src/CloseGame.js
@@ -16,7 +16,7 @@ const CloseGame = ({ awayTeam, homeTeam, awayId, homeId, teams }) => {
         <Card>
             <Card.Body>
             <div className="teamName"><img src={getImage(awayId)}  alt="Away team logo" className="team-logo" />{awayTeam}</div>
-            <div className="vs">vs</div>
+            <div className="vs">at</div>
             <div className="teamName"><img src={getImage(homeId)} alt="Home team logo" className="team-logo" />{homeTeam}</div>
             </Card.Body>
         </Card>
